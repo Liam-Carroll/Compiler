d3c95af6e82cea18bf0df84a686c4aa083a7156b
Kyle Corga <82676455+KyleCorga@users.noreply.github.com>
2021-10-25 20:52:12 -0400

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -78,10 +78,16 @@ public class Parser{
     private void parameter_list() {
 
     }
-    private void compound_statement() {
-        
+    private void compound_statement() throws Exception {
+        if (symbol.tokenType == T.BEGIN) {
+        	statement_list();
+        	symbol = s.nextToken();
+        	if (symbol.tokenType != T.END) {
+        		s.customError("ERROR -- EXPECTING END", s.line);
+        	}
+        }
     }
-    private void statement_list() {
+    private void statement_list() { // ----------------------------------------------idk what goes in here?
         
     }
     private void statement() throws Exception { // not done
@@ -90,6 +96,21 @@ public class Parser{
         }
         else if (symbol.tokenType == T.CALL) {
         	procedure_statement();
+        }
+        else if (symbol.tokenType == T.BEGIN) {
+        	compound_statement();
+        }
+        else if (symbol.tokenType == T.IF) { 
+        	if_statement();
+        }
+        else if (symbol.tokenType == T.WHILE) {
+        	while_statement();
+        }
+        else if (symbol.tokenType == T.READ) {
+        	read_statement();
+        }
+        else if (symbol.tokenType == T.WRITE) {
+        	write_statement();
         }
        
     }
@@ -101,13 +122,25 @@ public class Parser{
             }
     	}
     }
-    private void if_statement() {
-        
+    private void if_statement() throws Exception { // -------------------------------------------- liam check this, the else brackets is that right?
+        if (symbol.tokenType == T.IF) {
+        	expression();
+        	symbol = s.nextToken();
+        	if (symbol.tokenType == T.THEN) {
+        		statement();
+        		symbol = s.nextToken();
+        		if (symbol.tokenType == T.ELSE) {
+        			statement();
+        		}
+        	} else {
+        		s.customError("ERROR -- EXPECTING THEN", s.line);
+        	}
+        }
     }
     private void while_statement() {
         
     }
-    private void procedure_statement() throws Exception { // not done
+    private void procedure_statement() throws Exception {
         if (symbol.tokenType == T.CALL) {
         	symbol = s.nextToken();
         	if (symbol.tokenType == T.IDENTIFIER) {
@@ -125,20 +158,34 @@ public class Parser{
     private void expression_list() {
         
     }
-    private void expression() {
-        
+    private void expression() throws Exception {
+    	symbol = s.nextToken();
+    	simple_expression();
+        //if (symbol.tokenType == T.RE) // --------------------------------------------- NO T.RELOP WHAT DO HELP
     }
-    private void simple_expression() {
-        
+    private void simple_expression() throws Exception { // -------------------------- confused about the minus here. is this correct?
+        symbol = s.nextToken();
+        term();
+        symbol = s.nextToken();
+        while (symbol.tokenType == T.PLUS) {
+        	term();
+        	symbol = s.nextToken();
+        }
     }
-    private void term() {
-        
+    private void term() throws Exception {
+        symbol = s.nextToken();
+        factor();
+        symbol = s.nextToken();
+        while (symbol.tokenType == T.TIMES) {
+        	factor();
+        	symbol = s.nextToken();
+        }
     }
     private void factor() {
         
     }
     private void read_statement() {
-        
+      
     }
     private void write_statement() {
         
